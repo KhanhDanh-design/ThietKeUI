afcaee210bc956d964149b5875c643d796df52ff
NH1Tkendo <ngobatai1711@gmail.com>
2025-05-11 10:19:18 +0700

    Update

--- FigNex_Mobile/app/src/main/java/vn/edu/dlu/ctk46/fignex_mobile/LoginFrg.java
+++ FigNex_Mobile/app/src/main/java/vn/edu/dlu/ctk46/fignex_mobile/LoginFrg.java
@@ -1,6 +1,73 @@
 package vn.edu.dlu.ctk46.fignex_mobile;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class LoginFrg extends Fragment {
+    private EditText edtUser,edtPass;
+    private Button btnDangKy;
+    private TextView s;
+    private Context mContext;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.login_frg, container, false);
+        initView(rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String text = "Chưa có tài khoản? Đăng kí";
+        SpannableString spannable = new SpannableString(text);
+
+        spannable.setSpan(new ForegroundColorSpan(Color.parseColor("#EF672D")),
+                text.indexOf("Đăng kí"), text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        s.setText(spannable);
+
+        s.setOnClickListener(v -> {
+            gotoRegisterScreen();
+        });
+
+        btnDangKy.setOnClickListener(v -> {
+            gotoMainScreen();
+        });
+
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+    private void initView(View v) {
+        edtUser = v.findViewById(R.id.username);
+        edtPass = v.findViewById(R.id.password);
+        btnDangKy = v.findViewById(R.id.btn_register);
+        s = v.findViewById(R.id.tv_login);
+    }
+
+    private void gotoRegisterScreen() {
+        ((MainActivity) mContext).gotoRegisterScreen();
+    }
+
+    private void gotoMainScreen() {
+        ((MainActivity) mContext).gotoMainScreen();
+    }
 }
--- FigNex_Mobile/app/src/main/java/vn/edu/dlu/ctk46/fignex_mobile/M001HomePageFrg.java
+++ FigNex_Mobile/app/src/main/java/vn/edu/dlu/ctk46/fignex_mobile/M001HomePageFrg.java
@@ -1,6 +1,46 @@
 package vn.edu.dlu.ctk46.fignex_mobile;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class M001HomePageFrg extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.edu.dlu.ctk46.fignex_mobile.databinding.M001HomepageFrgBinding;
+
+public class M001HomePageFrg extends Fragment{
+    private M001HomepageFrgBinding binding;
+    private MovieAdapter adapter;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = M001HomepageFrgBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        List<Integer> imageList = new ArrayList<>();
+        imageList.add(R.drawable.nowayhome);
+
+        // Cấu hình RecyclerView
+        GridLayoutManager movieManager = new GridLayoutManager(
+                getContext(),
+                1, // số dòng muốn hiển thị
+                GridLayoutManager.HORIZONTAL, // cuộn ngang
+                false
+        );
+
+        binding.recyclerTrending.setLayoutManager(movieManager);
+        adapter = new MovieAdapter(imageList);
+        binding.recyclerTrending.setAdapter(adapter);
+    }
 }
